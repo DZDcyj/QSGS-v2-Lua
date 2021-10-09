3700a60a1c26e3c9f9f491a2086f95d0cc3f67bb
Rinsan <cyjdtxz@gmail.com>
2021-10-09 15:03:55 +0800

    feat: 添加神甘宁【万人辟易】皮肤

--- image/heroskin/lang/zh_CN/Audio/StandardGeneralPackageLines.lua
+++ image/heroskin/lang/zh_CN/Audio/StandardGeneralPackageLines.lua
@@ -1,5 +1,5 @@
 return {
-	-- 甄姬
+    -- 甄姬
     ['$qingguo1-zhenji_1'] = '进止难期，若往若还。',
     ['$qingguo2-zhenji_1'] = '动无常则，若危若安。',
     ['$luoshen1-zhenji_1'] = '翩若惊鸿，婉若游龙。',
@@ -7,61 +7,61 @@ return {
     ['~zhenji-zhenji_1'] = '揽騑辔以抗策，怅盘桓而不能去。',
     ['$qingguo1-zhenji_2'] = '含辞未吐，气若幽兰~',
     ['$luoshen2-zhenji_3'] = '飘飘兮若流风之回雪。',
-	-- 诸葛亮
+    -- 诸葛亮
     ['$guanxing1-zhugeliang_1'] = '诚如是，则汉室可兴矣。',
     ['$guanxing2-zhugeliang_1'] = '知天易，逆天难。',
     ['$kongcheng1-zhugeliang_1'] = '挫敌锐气，静待反击之时！',
     ['~zhugeliang-zhugeliang_1'] = '悠悠苍天,曷此其极！',
-	-- 貂蝉
+    -- 貂蝉
     ['$lijian1-diaochan_1'] = '将军，那人对妾身，好生无礼~',
     ['$biyue1-diaochan_1'] = '让妾身为您献上一舞！',
     ['~diaochan-diaochan_1'] = '红颜多薄命，几人能白头！',
-	-- 司马懿
+    -- 司马懿
     ['$nosfankui1-nos_simayi_2'] = '汝之愚行，正中下怀。',
     ['$nosguicai2-nos_simayi_2'] = '我命由我不由天。',
     ['~nos_simayi-nos_simayi_2'] = '难道真的是天命难违？',
-	-- 夏侯惇
+    -- 夏侯惇
     ['$nosganglie1-nos_xiahoudun_1'] = '鼠辈，胆敢伤我！',
     ['~nos_xiahoudun-nos_xiahoudun_1'] = '血染沙场，马革裹尸，快哉，快哉……',
-	-- 张辽
+    -- 张辽
     ['$nostuxi1-nos_zhangliao_1'] = '没想到吧？！',
     ['$nostuxi2-nos_zhangliao_1'] = '切入敌阵，先发制人！',
     ['~nos_zhangliao-nos_zhangliao_1'] = '身死名亦在，余威震江东……',
-	-- 许褚
+    -- 许褚
     ['$nosluoyi1-nos_xuchu_1'] = '谁来和我大战三百回合 ！',
     ['~nos_xuchu-nos_xuchu_1'] = '难保主周全，留此躯何用……',
-	-- 郭嘉
+    -- 郭嘉
     ['$nosyiji1-nos_guojia_1'] = '如此…也好！',
     ['$nosyiji2-nos_guojia_1'] = '吾尚有一策！',
     ['$nosyiji3-nos_guojia_1'] = '幸君提点，大计已成。',
     ['$tiandu3-nos_guojia_1'] = '天意弄人，罢了！',
     ['$tiandu4-nos_guojia_1'] = '若如此，也无可奈何……',
     ['~nos_guojia-nos_guojia_1'] = '难道真的是天命难违？',
-	-- 刘备
+    -- 刘备
     ['$nosrende1-nos_liubei_1'] = '惟贤惟德，仁服于人。',
     ['$nosrende2-nos_liubei_1'] = '以德服人。',
     ['~nos_liubei-nos_liubei_1'] = '难道这就是桃园吗……',
-	-- 关羽
+    -- 关羽
     ['$wusheng4-nos_guanyu_1'] = '观尔乃插标卖首。',
     ['~nos_guanyu-nos_guanyu_1'] = '难道这就是桃园吗……',
     ['$paoxiao4-nos_zhangfei_1'] = '匹夫，再吃我一矛',
     ['$longdan4-nos_zhaoyun_1'] = '赤胆平乱世，龙枪定江山！',
-	-- 黄月英
+    -- 黄月英
     ['$nosjizhi1-nos_huangyueying_1'] = '还记得我给你的锦囊吗？',
     ['$nosjizhi2-nos_huangyueying_1'] = '尽在我们掌握之中。',
     ['$nosjizhi3-nos_huangyueying_1'] = '一起度过这道难关吧！',
     ['~nos_huangyueying-nos_huangyueying_1'] = '孔明大人，请一定要赢~',
     ['$nosjizhi2-nos_huangyueying_2'] = '如何？',
     ['~nos_huangyueying-nos_huangyueying_2'] = '计虽不成亦无悔，不负天下不负卿。',
-	-- 孙尚香
+    -- 孙尚香
     ['$jieyin1-sunshangxiang_1'] = '将军暂歇，聆歌一曲。',
     ['$jieyin2-sunshangxiang_1'] = '举案齐眉意，愿君知吾心。',
     ['$xiaoji1-sunshangxiang_1'] = '休要小看我~~',
     ['~sunshangxiang-sunshangxiang_1'] = '不，我不会输……',
-	-- 孙权
+    -- 孙权
     ['$jiuyuan2-sunquan_1'] = '顿感雄师百万来援！',
     ['~sunquan-sunquan_1'] = '终究还是不如父亲和大哥……',
-	-- 界徐盛
+    -- 界徐盛
     ['$LuaPojun1-JieXusheng_1'] = '以攻为守，破敌卫疆！',
     ['$LuaPojun2-JieXusheng_1'] = '这长江天险后，便是江东铁壁！',
     ['~JieXusheng-JieXusheng_1'] = '望吾死后，江东依然无人敢犯……',
@@ -73,4 +73,10 @@ return {
     ['$LuaRangjie1-ExYangbiao_1'] = '百般忍让，只求皇上不受波及',
     ['$LuaRangjie2-ExYangbiao_1'] = '天下动之至易，安之甚难，惟明公虑焉',
     ['~ExYangbiao-ExYangbiao_1'] = '朝代更迭，难道真是天意难改……',
+    -- 神甘宁
+    ['$jieyingy1-shenganning_1'] = '劫敌营寨，以破其胆！',
+    ['$jieyingy2-shenganning_1'] = '百骑劫魏营，功震天下英！',
+    ['$poxi1-shenganning_1'] = '战胜勍敌，展江东豪杰之魄！',
+    ['$poxi2-shenganning_1'] = '此次进击，定要丧敌胆魄！',
+    ['~shenganning-shenganning_1'] = '神鸦不佑，此身竟陨……'
 }
