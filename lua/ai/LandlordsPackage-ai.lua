0c82fb79deef5359aebeb6982129e84f82f5182c
Rinsan <cyjdtxz@gmail.com>
2021-10-09 12:48:08 +0800

    feat: 添加斗地主【飞扬】 AI

---
+++ lua/ai/LandlordsPackage-ai.lua
@@ -0,0 +1,19 @@
+-- 飞扬 AI
+sgs.ai_skill_discard['LuaFeiyang'] = function(self, discard_num, min_num, optional, include_equip)
+    -- 如果判定区内有“言笑”牌，不发动飞扬
+    if self.player:containsTrick('YanxiaoCard') then
+        return {}
+    end
+    -- 引用“修罗”逻辑
+    if
+        not self.player:containsTrick('indulgence') and not self.player:containsTrick('supply_shortage') and
+            not (self.player:containsTrick('lightning') and not self:hasWizard(self.enemies))
+     then
+        return {}
+    end
+    -- 丢弃使用价值最低的两张手牌
+    local cards = self.player:getCards('h')
+    cards = sgs.QList2Table(cards)
+    self:sortByUseValue(cards, true)
+    return {cards[1]:getEffectiveId(), cards[2]:getEffectiveId()}
+end
