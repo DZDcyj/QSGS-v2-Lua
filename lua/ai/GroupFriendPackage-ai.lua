94a4318a7ef81ad8006d212850562419f485a74e
Rinsan <cyjdtxz@gmail.com>
2021-10-15 23:10:01 +0800

    feat: 添加群友包 AI

--- lua/ai/GroupFriendPackage-ai.lua
+++ lua/ai/GroupFriendPackage-ai.lua
@@ -49,3 +49,152 @@ sgs.ai_skill_discard['LuaShaika'] = function(self, discard_num, min_num, optiona
     end
     return to_discard
 end
+
+local LuaSoutu_skill = {}
+LuaSoutu_skill.name = 'LuaSoutuVS'
+
+table.insert(sgs.ai_skills, LuaSoutu_skill)
+
+LuaSoutu_skill.getTurnUseCard = function(self, inclusive)
+    if not self.player:hasUsed('#LuaSoutuCard') then
+        return sgs.Card_Parse('#LuaSoutuCard:.:')
+    end
+end
+
+sgs.ai_skill_use_func['#LuaSoutuCard'] = function(card, use, self)
+    local target = nil
+    if self.player:isKongcheng() then
+        return
+    end
+    for _, friend in ipairs(self.friends_noself) do
+        if friend:hasSkill('LuaSoutu') then
+            target = friend
+            break
+        end
+    end
+    if target then
+        local cards = self.player:getHandcards()
+        cards = sgs.QList2Table(cards)
+        self:sortByUseValue(cards, true)
+        local card = nil
+        for _, card1 in ipairs(cards) do
+            card = card1
+            break
+        end
+        local card_str = string.format('#LuaSoutuCard:%s:', card:getEffectiveId())
+        local acard = sgs.Card_Parse(card_str)
+        assert(acard)
+        use.card = acard
+        if use.to then
+            use.to:append(target)
+        end
+    end
+end
+
+sgs.ai_use_value['LuaSoutuCard'] = 100
+sgs.ai_use_priority['LuaSoutuCard'] = 10
+
+sgs.ai_skill_playerchosen.LuaMouhai = function(self, targetlist)
+    local targets = sgs.QList2Table(targetlist)
+    self:sort(targets)
+    local friends, enemies = {}, {}
+    for _, target in ipairs(targets) do
+        if not self:cantbeHurt(target, self.player) and self:damageIsEffective(target, nil, self.player) then
+            if self:isEnemy(target) then
+                table.insert(enemies, target)
+            elseif self:isFriend(target) then
+                table.insert(friends, target)
+            end
+        end
+    end
+    for _, enemy in ipairs(enemies) do
+        if not self:getDamagedEffects(enemy, self.player) and not self:needToLoseHp(enemy, self.player) then
+            return enemy
+        end
+    end
+    for _, friend in ipairs(friends) do
+        if self:getDamagedEffects(friend, self.player) and self:needToLoseHp(friend, self.player) then
+            return friend
+        end
+    end
+    return nil
+end
+
+sgs.ai_skill_invoke.LuaJuesha = function(self, data)
+    local target = data:toPlayer()
+    if not self:isFriend(target) then
+        return true
+    end
+    return false
+end
+
+sgs.ai_skill_playerchosen.LuaChuanyi = function(self, targetlist)
+    -- AI 永不发动传艺
+    return nil
+end
+
+sgs.ai_skill_cardask['@LuaYinyu-show'] = function(self, data)
+    local target = data:toPlayer()
+    if target:objectName() == self.player:objectName() then
+        local cards = self.player:getCards('h')
+        cards = sgs.QList2Table(cards)
+        self:sortByUseValue(cards, true)
+        return cards[1]
+    end
+    if not self:isFriend(target) then
+        return '.'
+    end
+    if self:isWeak() then
+        return '.'
+    end
+    if self.player:getHandcardNum() <= math.min(self.player:getMaxCards(), 2) then
+        return '.'
+    end
+    local cards = self.player:getCards('h')
+    cards = sgs.QList2Table(cards)
+    self:sortByUseValue(cards, true)
+    return cards[1]
+end
+
+sgs.ai_skill_invoke.LuaYinyu = function(self, data)
+    local target = data:toPlayer()
+    if self:isFriend(target) then
+        return true
+    end
+    return false
+end
+
+sgs.ai_skill_invoke.LuaPaozhuan = function(self, data)
+    local target = data:toPlayer()
+    if self:isFriend(target) then
+        return false
+    end
+    return true
+end
+
+sgs.ai_skill_invoke.LuaZhazhi = function(self, data)
+    local target = data:toPlayer()
+    if self:isFriend(target) then
+        return false
+    end
+    if
+        self:getCardsNum('Slash', target) < 1 and self.player:getHp() > 1 and not self:canHit(self.player, target) and
+            not (target:hasWeapon('double_sword') and self.player:getGender() ~= target:getGender())
+     then
+        return true
+    end
+    if
+        sgs.card_lack[target:objectName()]['Slash'] == 1 or self:needLeiji(self.player, target) or
+            self:getDamagedEffects(self.player, target, true) or
+            self:needToLoseHp(self.player, target, true)
+     then
+        return true
+    end
+    if self:getOverflow() and self:getCardsNum('Jink') > 1 then
+        return true
+    end
+    if target:isWeak() then
+        return true
+    end
+    return not self:isWeak()
+end
