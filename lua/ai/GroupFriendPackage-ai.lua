2fedb324acc4f8e7ca7a0e03f4b75be39f4319b2
Rinsan <cyjdtxz@gmail.com>
2021-11-19 13:58:55 +0800

    refactor: 移除多余空行

--- lua/ai/GroupFriendPackage-ai.lua
+++ lua/ai/GroupFriendPackage-ai.lua
@@ -2,7 +2,6 @@
 -- Created by DZDcyj at 2021/9/10
 
 -- 晒卡弃牌/掉血
-
 sgs.ai_skill_discard['LuaShaika'] = function(self, discard_num, min_num, optional, include_equip)
     -- 如果要弃置的牌大于总牌数，则不弃牌
     if discard_num > self.player:getCardCount(true) then
