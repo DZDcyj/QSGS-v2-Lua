70aa53163dc3b9cb27c05124c9095a5310e80fa9
Rinsan <cyjdtxz@gmail.com>
2021-11-19 14:02:56 +0800

    feat: 添加 AI 伤害判断 (#368)
    * feat: 添加 AI 伤害判断
    
    * fix: 修复 SmartAI 不能正确触发函数回调的问题
    
    * 注意：需要修改 smart-ai 文件，在 SmartAI 文件中 sgs.ai_damage_effect 回调中传入 damage 参数
    
    * fix: 修复静态扫描问题
    
    * refactor: 移除多余空行

--- lua/ai/GroupFriendPackage-ai.lua
+++ lua/ai/GroupFriendPackage-ai.lua
@@ -130,7 +130,6 @@ sgs.ai_skill_invoke.LuaJuesha = function(self, data)
     return false
 end
 
-
 -- 传艺
 sgs.ai_skill_choice['LuaChuanyi'] = function(self, choices)
     -- 选最后一个：本局游戏不再发动
@@ -211,6 +210,20 @@ sgs.ai_skill_invoke.LuaZhazhi = function(self, data)
     end
     return not self:isWeak()
 end
+
+-- 榨汁伤害判断
+LuaZhazhiDamageEffect = function(self, to, nature, from, damageValue)
+    local count = damageValue
+    for _, mark in sgs.list(from:getMarkNames()) do
+        if string.find(mark, 'LuaZhazhiDebuff')  then
+            count = count - from:getMark(mark)
+        end
+    end
+    return count > 0
+end
+
+-- 不知道为什么不能直接定义 sgs.ai_damage_effect 对应的函数，只能如这般插入
+table.insert(sgs.ai_damage_effect, LuaZhazhiDamageEffect)
 
 -- 白嫖
 sgs.ai_skill_playerchosen.LuaBaipiao = function(self, targetlist)
@@ -336,3 +349,13 @@ sgs.ai_skill_use['@@LuaZhixie'] = function(self, prompt, method)
     end
     return '.'
 end
+
+-- 机械减伤害
+LuaJixieDamageEffect = function(self, to, nature, from, damageValue)
+    if to:hasSkill('LuaJixie') and nature == sgs.DamageStruct_Thunder then
+        return damageValue > 1
+    end
+    return true
+end
+
+table.insert(sgs.ai_damage_effect, LuaJixieDamageEffect)
