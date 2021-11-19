7aac3460c8ad75a02347f511e189a4886c1b938f
Rinsan <cyjdtxz@gmail.com>
2021-11-19 12:39:43 +0800

    fix: 修复 SmartAI 不能正确触发函数回调的问题
    * 注意：需要修改 smart-ai 文件，在 SmartAI 文件中 sgs.ai_damage_effect 回调中传入 damage 参数

--- lua/ai/GroupFriendPackage-ai.lua
+++ lua/ai/GroupFriendPackage-ai.lua
@@ -2,6 +2,10 @@
 -- Created by DZDcyj at 2021/9/10
 
 -- 晒卡弃牌/掉血
+
+-- 不知道为什么不能直接定义 sgs.ai_damage_effect 对应的函数，先用一个临时存储，再插入
+local group_ai_damage_effect = {}
+
 sgs.ai_skill_discard['LuaShaika'] = function(self, discard_num, min_num, optional, include_equip)
     -- 如果要弃置的牌大于总牌数，则不弃牌
     if discard_num > self.player:getCardCount(true) then
@@ -211,15 +215,18 @@ sgs.ai_skill_invoke.LuaZhazhi = function(self, data)
     return not self:isWeak()
 end
 
--- 考虑到 SmartAI 中仅传入这些参数，故暂时仅判断是否榨汁-1
-sgs.ai_damage_effect['LuaZhazhi'] = function(self, to, nature, from)
-    for _, mark in sgs.list(to:getMarkNames()) do
-        if string.find(mark, 'LuaZhazhiDebuff') and to:getMark(mark) > 0 then
-            return false
+-- 榨汁伤害判断
+LuaZhazhiDamageEffect = function(self, to, nature, from, damageValue)
+    local count = damageValue
+    for _, mark in sgs.list(from:getMarkNames()) do
+        if string.find(mark, 'LuaZhazhiDebuff')  then
+            count = count - from:getMark(mark)
         end
     end
-    return true
+    return count > 0
 end
+
+table.insert(sgs.ai_damage_effect, LuaZhazhiDamageEffect)
 
 -- 白嫖
 sgs.ai_skill_playerchosen.LuaBaipiao = function(self, targetlist)
@@ -347,10 +354,11 @@ sgs.ai_skill_use['@@LuaZhixie'] = function(self, prompt, method)
 end
 
 -- 机械减伤害
-sgs.ai_damage_effect['LuaJixie'] = function(self, to, nature, from)
-    self.room:writeToConsole(from:getGeneralName() .. ' is trying to damage ' .. to:getGeneralName())
+LuaJixieDamageEffect = function(self, to, nature, from, damageValue)
     if to:hasSkill('LuaJixie') and nature == sgs.DamageStruct_Thunder then
-        return false
+        return damageValue > 1
     end
     return true
 end
+
+table.insert(sgs.ai_damage_effect, LuaJixieDamageEffect)
