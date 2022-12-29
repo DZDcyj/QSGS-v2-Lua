5e219e9e0cacfa451b3073ace3c5a8093fb16d51
Rinsan <cyjdtxz@gmail.com>
2022-12-29 10:38:41 +0800

    feat: 添加【奇正相生】卡牌

---
+++ extensions/ExpansionCardPackage.lua
@@ -0,0 +1,77 @@
+-- 扩展卡牌包
+-- Created by DZDcyj at 2022/12/29
+module('extensions.ExpansionCardPackage', package.seeall)
+extension = sgs.Package('ExpansionCardPackage', sgs.Package_CardPack)
+
+-- 引入封装函数包
+local rinsan = require('QSanguoshaLuaFunction')
+
+-- 奇正相生
+indirect_combination = sgs.CreateTrickCard {
+    name = 'indirect_combination',
+    class_name = 'IndirectCombination',
+    subtype = 'single_target_trick',
+    target_fixed = false,
+    can_recast = false,
+    is_cancelable = true,
+    filter = function(self, selected, to_select)
+        return rinsan.checkFilter(selected, to_select, rinsan.EQUAL, 0)
+    end,
+    feasible = function(self, targets)
+        return #targets == 1
+    end,
+    on_use = function(self, room, source, targets)
+        for _, target in ipairs(targets) do
+            room:cardEffect(self, source, target)
+        end
+    end,
+    on_effect = function(self, effect)
+        local source = effect.from
+        local target = effect.to
+        local room = source:getRoom()
+        local data = sgs.QVariant()
+        data:setValue(effect)
+        -- 正兵或奇兵
+        local choice = room:askForChoice(source, self:objectName(), 'Direct+Indirect', data)
+        local card = room:askForCard(target, 'Slash,Jink|.|.|.',
+            string.format('indirect_combination-card:%s', source:objectName()), sgs.QVariant(), sgs.Card_MethodResponse)
+        rinsan.sendLogMessage(room, '#choose', {
+            ['from'] = source,
+            ['arg'] = choice
+        })
+        if choice == 'Direct' then
+            -- 正兵
+            if (not card) or (not card:isKindOf('Jink')) then
+                if target:isNude() then
+                    return
+                end
+                rinsan.sendLogMessage(room, '#DirectFailed', {
+                    ['from'] = source,
+                    ['to'] = target,
+                    ['arg'] = 'jink'
+                })
+                local card_id = room:askForCardChosen(source, target, 'he', self:objectName(), false,
+                    sgs.Card_MethodNone)
+                local reason = sgs.CardMoveReason(sgs.CardMoveReason_S_REASON_EXTRACTION, source:objectName())
+                room:obtainCard(source, sgs.Sanguosha:getCard(card_id), reason, false)
+            end
+        elseif choice == 'Indirect' then
+            -- 奇兵
+            if (not card) or (not card:isKindOf('Slash')) then
+                rinsan.sendLogMessage(room, '#IndirectFailed', {
+                    ['from'] = source,
+                    ['to'] = target,
+                    ['arg'] = 'slash'
+                })
+                rinsan.doDamage(room, source, target, 1)
+            end
+        end
+    end
+}
+
+for i = 2, 9, 1 do
+    local card = indirect_combination:clone()
+    card:setSuit((i % 2 == 0) and sgs.Card_Spade or sgs.Card_Club)
+    card:setNumber(i)
+    card:setParent(extension)
+end
---
+++ lang/zh_CN/Package/ExpansionCardPackage.lua
@@ -0,0 +1,14 @@
+-- translation for ExpansionCard Package
+
+return {
+    ['ExpansionCardPackage'] = '扩展卡牌包',
+    ['indirect_combination'] = '奇正相生',
+    [':indirect_combination'] = '锦囊牌<br /><b>时机</b>：出牌阶段<br /><b>目标</b>：一名其他角色<br /><b>效果</b>：你为其指定正兵或奇兵，然后其可以打出一张【杀】或【闪】\
+    若奇兵目标没有打出【杀】，你对其造成1点伤害\
+    若正兵目标没有打出【闪】，你获得其一张牌',
+    ['indirect_combination-card'] = '%src 使用了【奇正相生】，请打出一张【杀】或【闪】',
+    ['Direct'] = '正兵',
+    ['Indirect'] = '奇兵',
+    ['#DirectFailed'] = '%to 没有打出【%arg】，%from 将获得其一张牌',
+    ['#IndirectFailed'] = '%to 没有打出【%arg】，%from 将对其造成一点伤害'
+}
