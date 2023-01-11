d5cbdda72f61c5a8b070bb68a0008a5a5de03ab0
Cactusssssssss <595045507@qq.com>
2023-01-11 19:47:59 +0800

    fix: 添加了大妖的语音包

--- image/heroskin/lang/zh_CN/Audio/StandardGeneralPackageLines.lua
+++ image/heroskin/lang/zh_CN/Audio/StandardGeneralPackageLines.lua
@@ -203,8 +203,19 @@ return {
     ['$LuaTianzuo1-ExShenXunyu_1'] = '这位更是脑子不太好——孙策',
     ['$LuaTianzuo2-ExShenXunyu_1'] ='这位更是脑子不太好——孙笨',
     ['$LuaLingce1-ExShenXunyu_1'] = '这位更是脑子不太好——孙枣',
-    ['$LuaLingce2-ExShenXunyu_1'] ='这位更是脑子不太好——孙厕',
-    ['$LuaDinghan1-ExShenXunyu_1'] = '这位更是脑子不太好——江东小霸王',
-    ['$LuaDinghan2-ExShenXunyu_1'] = '这位更是脑子不太好——江东小白板',
+    ['$LuaLingce2-ExShenXunyu_1'] ='这位更是脑子不太好——孙束',
+    ['$LuaDinghan1-ExShenXunyu_1'] = '这位更是脑子不太好——江东小白板',
+    ['$LuaDinghan2-ExShenXunyu_1'] = '这位更是脑子不太好——辣个男人',
     ['~ExShenXunyu-ExShenXunyu_1'] = '一定是王粲干的',
+    -- 神郭嘉
+    ['$LuaHuishi1-ExShenguojia_1'] = '机关精巧，将军可看在眼里',
+    ['$LuaHuishi2-ExShenguojia_1'] = '区区小技，有手就行',
+    ['$LuaTianyi1-ExShenguojia_1'] = '主公以国事待吾，吾食主公心智',
+    ['$LuaTianyi2-ExShenguojia_1'] = '虽然付出了许多代价，但是收获也不错，主公死了（喜）',
+    ['$LuaHuishiLimit1-ExShenguojia_1'] = '伐吴此举十有九利，惟陛下察之',
+    ['$LuaHuishiLimit2-ExShenguojia_1'] = '我早知你小子天命不僣，果真如我所料',
+    ['$LuaZuoxing1-ExShenguojia_1'] = '倾荡芝士已成，石城尽在眼下',
+    ['$LuaZuoxing2-ExShenguojia_1'] = '吾军势如破竹，江东六郡唾手可得',
+    ['~ExShenGuojia-ExShenguojia_1'] = '我俏，我他妈死了是吧',
+
 }
