1b89a2ae0f19f7e6e29debe9e9ba17c58ba78750
Rinsan <cyjdtxz@gmail.com>
2023-02-07 16:03:37 +0800

    fix: 标记清除与语音错别字修正

--- lang/zh_CN/Audio/ShieldPackageLines.lua
+++ lang/zh_CN/Audio/ShieldPackageLines.lua
@@ -4,7 +4,7 @@ return {
     -- 谋吕蒙
     ['$LuaMouKeji1'] = '事事克己，步步虚心！',
     ['$LuaMouKeji2'] = '勤学潜习，始觉自新！',
-    ['$LuaMouDujiang1'] = '大军渡江，昼夜驰上！',
+    ['$LuaMouDujiang1'] = '大军浮江，昼夜驰上！',
     ['$LuaMouDujiang2'] = '白衣摇橹，昼夜兼行！',
     ['$LuaMouDuojing1'] = '快舟轻甲，速袭其后！',
     ['$LuaMouDuojing2'] = '复取荆州，尽在掌握！',
