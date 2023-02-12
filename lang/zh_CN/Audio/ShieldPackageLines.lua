c7f2f9bcd678f333448adfe9f05f9a895f555192
Rinsan <cyjdtxz@gmail.com>
2023-02-12 18:21:53 +0800

    feat: 添加谋孙尚香 (#617)
    * feat: 谋孙尚香基础
    
    * feat: 语音图片资源
    
    * refactor: 修正静态代码检查问题
    
    * fix: 调整文本
    
    * fix: 修正文本显示问题
    
    * fix: 修正场上可弃牌范围
    
    * fix: AI 微调
    
    * fix: 文本严谨化

--- lang/zh_CN/Audio/ShieldPackageLines.lua
+++ lang/zh_CN/Audio/ShieldPackageLines.lua
@@ -20,5 +20,13 @@ return {
     ['$LuaMouYaowu2'] = '上将潘凤？哼！还不是死在我刀下！',
     ['$LuaMouYangwei1'] = '哈哈哈哈！现在谁不知我华雄？',
     ['$LuaMouYangwei2'] = '定要关外诸侯，知我威名！',
-    ['~ExMouHuaxiong'] = '小小马弓手，竟然……啊……'
+    ['~ExMouHuaxiong'] = '小小马弓手，竟然……啊……',
+    -- 谋孙尚香
+    ['$LuaMouLiangzhu1'] = '助君得胜战，跃马提缨枪！',
+    ['$LuaMouLiangzhu2'] = '平贼成君业，何惜上沙场！',
+    ['$LuaMouJieyin1'] = '君若不负吾心，妾自随君千里！',
+    ['$LuaMouJieyin2'] = '夫妻之情既断，何必再问归期！',
+    ['$LuaMouXiaoji1'] = '吾之所通，何止十八般兵刃！',
+    ['$LuaMouXiaoji2'] = '既如此，就让尔等见识一番！',
+    ['~ExMouSunshangxiang'] = '此去一别，竟无再见之日……'
 }
