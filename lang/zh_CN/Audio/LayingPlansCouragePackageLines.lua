b62e56851e52a702f41b0e51e916940cb289c56f
Rinsan <cyjdtxz@gmail.com>
2023-02-15 20:21:57 +0800

    feat: 添加孙翊 (#626)
    * feat: 添加孙翊
    
    * refactor: 移除冗余初始值
    
    * fix: 部分细节修正
    
    * docs: 武将文档

---
+++ lang/zh_CN/Audio/LayingPlansCouragePackageLines.lua
@@ -0,0 +1,8 @@
+-- lines for LayingPlansCourage Package
+
+return {
+    -- 孙翊
+    ['$LuaZaoli1'] = '喜怒不形于色，诈伪要名之徒！',
+    ['$LuaZaoli2'] = '摇唇鼓舌，竖子是之也！',
+    ['~ExSunyi'] = '叛我贼子，虽死亦不饶之！'
+}
---
+++ lang/zh_CN/Package/LayingPlansCouragePackage.lua
@@ -0,0 +1,14 @@
+-- translation for LayingPlansCouragePackage
+
+return {
+    ['LayingPlansCouragePackage'] = '始计篇·勇包',
+    ['ExSunyi'] = '孙翊',
+    ['&ExSunyi'] = '孙翊',
+    ['#ExSunyi'] = '骁悍激躁',
+    ['LuaZaoli'] = '躁厉',
+    [':LuaZaoli'] = '锁定技，①你于出牌阶段内只能使用或打出本回合进入你手牌区的牌；\
+    ②当你使用或打出手牌时，若你的“厉”标记数小于4，你获得1个“厉”标记；\
+    ③回合开始时，若你有“厉”标记，你移去所有“厉”标记并弃置任意张牌（若有牌则至少弃置一张牌），然后你摸X张牌（X为你移去的“厉”标记数与弃置牌数之和）。若你移去的“厉”标记数大于2，你失去1点体力',
+    ['@LuaZaoli'] = '厉',
+    ['LuaZaoli-discard'] = '你须弃置至少一张牌'
+}
