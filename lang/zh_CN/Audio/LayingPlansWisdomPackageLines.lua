d43be8cdc30dbfa8d3a2ec12b6eb832d89943782
Rinsan <cyjdtxz@gmail.com>
2023-02-27 18:14:49 +0800

    refactor: 拆分始计篇-智包

---
+++ lang/zh_CN/Audio/LayingPlansWisdomPackageLines.lua
@@ -0,0 +1,44 @@
+-- lines for LayingPlans Wisdom Package
+
+return {
+    -- 杜预
+    ['$LuaWuku1'] = '损益万枢，竭世运机',
+    ['$LuaWuku2'] = '胸藏万卷，充盈如库',
+    ['$LuaSanchen1'] = '贼计已穷，陈兵吴地，可一鼓而下也',
+    ['$LuaSanchen2'] = '伐吴此举，十有九利，惟陛下察之',
+    ['$LuaMiewu1'] = '倾荡之势已成，石城尽在眼下！',
+    ['$LuaMiewu2'] = '吾军势如破竹，江东六郡唾手可得！',
+    ['~ExDuyu'] = '洛水圆石，遂道南向，吾将以俭自完耳',
+    -- 王粲
+    ['$LuaQiai1'] = '恨我无时谋，譬诸具官臣',
+    ['$LuaQiai2'] = '鞠躬中坚内，微画无所陈',
+    ['$LuaShanxi1'] = '连舫逾万艘，带甲千万人',
+    ['$LuaShanxi2'] = '率彼东南路，将定一举勋',
+    ['~ExWangcan'] = '虽无铅刀用，庶几奋薄身……',
+    -- 神荀彧
+    ['$LuaTianzuo1'] = '此时进之多弊，守之多利，愿主公熟虑！',
+    ['$LuaTianzuo2'] = '主公若不时定，待四方生心，则无及矣！',
+    ['$LuaLingce1'] = '绍士卒虽众，其实难用，必无为也',
+    ['$LuaLingce2'] = '袁军不过一盘沙砾，主公用奇则散',
+    ['$LuaDinghan1'] = '益国之事，虽死弗避',
+    ['$LuaDinghan2'] = '杀身有地，报国有时！',
+    ['~ExShenXunyu'] = '宁鸣而死，不默而生',
+    -- 神郭嘉
+    ['$LuaHuishi1'] = '聪以知远，明以察微',
+    ['$LuaHuishi2'] = '见微知著，识人心志',
+    ['$LuaTianyi1'] = '天命靡常，惟德是辅',
+    ['$LuaTianyi2'] = '可承吾志者，必此人也！',
+    ['$LuaLimitHuishi1'] = '丧家之犬，主公实不足虑也',
+    ['$LuaLimitHuishi2'] = '时事兼备，主公复有何忧？',
+    ['$LuaZuoxing1'] = '以聪虑难，悉咨于上',
+    ['$LuaZuoxing2'] = '奉孝不才，愿献勤心',
+    ['~ExShenGuojia'] = '可叹桢干命也迂',
+    -- 陈震
+    ['$LuaShameng1'] = '歃血盟誓，以告神明',
+    ['$LuaShameng2'] = '戮力一心，共讨魏贼！',
+    ['~ExChenzhen'] = '震，不负丞相所托…',
+    -- 骆统
+    ['$LuaQinzheng1'] = '治疾及其未笃，除患贵其未深',
+    ['$LuaQinzheng2'] = '夫国之有民，犹水之有舟，停则以安，扰则以危',
+    ['~ExLuotong'] = '臣统之大愿，足以死而不朽矣……',
+}
