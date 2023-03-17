38ec4d3be54b1db73267f5d206f4b1dac60e324b
Rinsan <cyjdtxz@gmail.com>
2023-03-18 00:27:08 +0800

    feat: 添加神姜维

---
+++ lang/zh_CN/Package/DevineForcePackage.lua
@@ -0,0 +1,21 @@
+-- translation for DevineForcePackage
+
+return {
+    ['DevineForcePackage'] = '神·武包',
+    ['ExTenYearShenJiangwei'] = '神姜维',
+    ['&ExTenYearShenJiangwei'] = '神姜维',
+    ['#ExTenYearShenJiangwei'] = '怒麟布武',
+    ['LuaTianren'] = '天任',
+    [':LuaTianren'] = '锁定技，①当一张或多张基本牌或普通锦囊牌不是因使用而置入弃牌堆后，你获得等量个“天任”标记；\
+    ②当你获得“天任”标记后，若“天任”标记数不小于你的体力上限，你移去体力上限数个“天任”标记，加1点体力上限并摸两张牌，然后你重复此流程直到“天任”标记数小于你的体力上限',
+    ['#LuaTianren'] = '%from 的“%arg”被触发，获得 %arg2 个“%arg”标记',
+    ['@LuaTianren'] = '天任',
+    ['LuaJiufa'] = '九伐',
+    [':LuaJiufa'] = '当你每累计使用或打出九张不同牌名的牌后，你可以亮出牌堆顶的九张牌，然后若其中有点数相同的牌，你选择并获得其中每个重复点数的牌各一张',
+    ['LuaPingxiang'] = '平襄',
+    [':LuaPingxiang'] = '限定技，出牌阶段，若你的体力上限大于9，你可以减9点体力上限，失去技能“九伐”且本局游戏内你的手牌上限等于体力上限。然后你视为使用至多九张不计入次数限制的【火杀】',
+    ['@LuaPingxiang'] = '平襄',
+    ['@LuaPingxiang-invoke'] = '你还可以发动至多 %arg 次“平襄”，视为使用一张不计入次数限制的【火杀】',
+    ['luapingxiang'] = '平襄',
+    ['~LuaPingxiang'] = '选择若干名合法角色→点击“确定”',
+}
