64da634cfcac5feb76ca4ecf483ad51fd98ab75d
Rinsan <cyjdtxz@gmail.com>
2023-04-13 18:02:13 +0800

    fix: 修正 commandType 错误问题

--- lua/QSanguoshaLuaFunction.lua
+++ lua/QSanguoshaLuaFunction.lua
@@ -2224,7 +2224,7 @@ ZHINANG_CARDS = {
 -- 手动修正
 FixedCommandType = {
     ['S_COMMAND_UPDATE_PILE'] = 58,
-    ['S_COMMAND_CHANGE_HP'] = 41,
+    ['S_COMMAND_CHANGE_HP'] = 31,
 }
 
 -- CardType 参数，用于 getCardMostProbably 方法
