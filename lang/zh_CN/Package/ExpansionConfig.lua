3a18191b6323ec6e4bd01c591f06a20ec65a1f2a
Rinsan <cyjdtxz@gmail.com>
2023-04-17 16:57:56 +0800

    fix: 系列修正 (#706)
    * fix: 修正斗地主势力选择
    
    * fix: 调整从游戏外或牌堆获取牌流程
    
    * fix: 调整流程，增加初始座次通报

--- lang/zh_CN/Package/ExpansionConfig.lua
+++ lang/zh_CN/Package/ExpansionConfig.lua
@@ -17,4 +17,10 @@ return {
     ['plpphskill'] = '出牌阶段每名角色各限一次',
     ['opprndskill'] = '每轮限一次，',
     ['oppplskill'] = '每名角色限一次',
+
+    ['choose_kingdom'] = '请选择一个势力',
+    ['#BroadcastSeat'] = '%arg 的座次为：%arg2',
+    ['BroadcastSeat1'] = '地主',
+    ['BroadcastSeat2'] = '第二个行动',
+    ['BroadcastSeat3'] = '第三个行动',
 }
