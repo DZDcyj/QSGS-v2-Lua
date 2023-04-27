ce0cce5bd9d4095f3d2115edba512197ec46caa9
Rinsan <cyjdtxz@gmail.com>
2023-04-27 18:31:09 +0800

    refactor: 移动蒲元到“限定-奇人异士包”

--- lang/zh_CN/Audio/ExpansionPackageLines.lua
+++ lang/zh_CN/Audio/ExpansionPackageLines.lua
@@ -307,10 +307,4 @@ return {
     ['$LuaGuanzong1'] = '汝为叔父，怎可与小辈计较！',
     ['$LuaGuanzong2'] = '阿瞒生龙活虎，汝切勿胡言！',
     ['~ExCaosong'] = '长恨人心不如水，等闲平地起波澜……',
-    -- 蒲元
-    ['$LuaZhuren1'] = '造刀三千口，用法各不同',
-    ['$LuaZhuren2'] = '此刀，可劈铁珠之筒！',
-    ['$LuaTianjiang1'] = '天赐匠法，精心锤炼！',
-    ['$LuaTianjiang2'] = '巧夺天工，超凡脱俗！',
-    ['~ExPuyuan'] = '铸木镂冰，怎成大器！',
 }
---
+++ lang/zh_CN/Package/ExtraordinaryPeoplePackage.lua
@@ -0,0 +1,15 @@
+-- translation for ExtraordinaryPeoplePackage
+
+return {
+    ['ExtraordinaryPeoplePackage'] = '限定-奇人异士包',
+    ['ExPuyuan'] = '蒲元',
+    ['&ExPuyuan'] = '蒲元',
+    ['#ExPuyuan'] = '淬炼百兵',
+    ['LuaTianjiang'] = '天匠',
+    [':LuaTianjiang'] = '①游戏开始时，你随机获得两张不同副类别的装备牌并置入你的装备区；\
+    ②出牌阶段，你可以将装备区里的牌移至其他角色的装备区（可以替换原装备），然后若此牌是“铸刃”的武器牌，你摸两张牌',
+    ['luatianjiang'] = '天匠',
+    ['LuaZhuren'] = '铸刃',
+    [':LuaZhuren'] = '出牌阶段限一次，你可以弃置一张手牌，打造一张武器牌（若打造失败或武器已有则改为获得一张【杀】，花色决定武器名称，点数决定成功率；当“铸刃”的武器牌进入弃牌堆时，将之移出游戏）',
+    ['luazhuren'] = '铸刃',
+}
