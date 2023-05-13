ee2664eb239d5bd0cfe2171dd690695db0ac9e1c
Cactusssssssss <70155752+Cactusssssssss@users.noreply.github.com>
2023-05-13 13:27:27 +0800

    fix: 调整图片和本地化文本 (#754)
    * fix: 修正了朱儁武将台词的问题，修改了大头小头武将皮肤的显示
    
    * fix: 朱儁拒降技能描述
    
    * fix: 再次修改张翼和界廖化的皮肤使之适应双将
    
    * feat: 添加苏飞皮肤
    
    * fix: 删除多余文件
    
    * fix: 调整界于禁武将图
    
    * fix: 苏飞皮肤

--- lang/zh_CN/Package/LayingPlansStrictnessPackage.lua
+++ lang/zh_CN/Package/LayingPlansStrictnessPackage.lua
@@ -10,7 +10,7 @@ return {
     ['luayangjie'] = '佯解',
     ['LuaYangjie-Choose'] = '你可以选择一名其他角色令其视为对 %src 使用一张【火杀】',
     ['LuaZhujunJuxiang'] = '拒降',
-    [':LuaZhujunJuxiang'] = '限定技，当其他角色脱离濒死状态时，你可以对其造成1点伤害',
+    [':LuaZhujunJuxiang'] = '限定技，当其他角色脱离濒死状态时，你可以对其造成1点伤害，然后摸X张牌（X为该角色的体力上限且至多为5）',
     ['@LuaZhujunJuxiang'] = '拒降',
     ['LuaHoufeng'] = '厚俸',
     [':LuaHoufeng'] = '每轮限一次，一名角色的出牌阶段开始时，若其在你攻击范围内，你可以令其选择一项“整肃”，然后本回合的弃牌阶段结束后，若“整肃”未失败，你与其均获得“整肃”奖励',
