c29c6ecb6f63550db6d01e8b9a91ca18975af882
Rinsan <cyjdtxz@gmail.com>
2023-09-10 23:40:06 +0800

    feat: 添加 OL 界庞统

--- lang/zh_CN/Audio/OLBoundaryBreachFirePackageLines.lua
+++ lang/zh_CN/Audio/OLBoundaryBreachFirePackageLines.lua
@@ -11,6 +11,18 @@ return {
     ['$LuaCangzhuo1'] = '藏巧于拙，用晦而明',
     ['$LuaCangzhuo2'] = '寓清于浊，以屈为伸',
     ['~JieWolong'] = '星途半废，夙愿未完……',
+    -- 界庞统
+    ['$LuaOLLianhuan1'] = '连环之策，攻敌之计',
+    ['$LuaOLLianhuan2'] = '锁链连舟，困步难行',
+    ['$LuaOLNiepan1'] = '烈火脱胎，涅槃重生',
+    ['$LuaOLNiepan2'] = '破而后立，方有大成',
+    ['$LuaBazhen3'] = '八卦四象，阴阳运转',
+    ['$LuaBazhen4'] = '离火艮山，皆随我用',
+    ['$LuaHuoji3'] = '火计诱敌，江水助势',
+    ['$LuaHuoji4'] = '火烧赤壁，曹贼必败',
+    ['$LuaKanpo3'] = '卧龙之才，吾也略懂',
+    ['$LuaKanpo4'] = '这些小伎俩，逃不出我的眼睛！',
+    ['~OLJiePangtong'] = '骥飞羽落，坡道归尘……',
     -- 界荀彧
     ['$LuaOLJieming1'] = '杀身成仁，不负皇恩',
     ['$LuaOLJieming2'] = '因势利导，是为良计',
