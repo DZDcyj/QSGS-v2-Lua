6a540146c9c1bcca2faa166adf07896fbd074533
Rinsan <cyjdtxz@gmail.com>
2023-09-30 23:49:27 +0800

    refactor: 拆分手杀-界一将成名 2013 武将包 (#846)
    * refactor: 拆分手杀-界一将成名 2013 武将包
    
    * fix: 修正界李儒语音并拆分皮肤

---
+++ lang/zh_CN/Audio/MobileJieYJCM2013PackageLines.lua
@@ -0,0 +1,22 @@
+-- lines for Mobile Jie YJCM 2013 Package
+
+return {
+    -- 界朱然
+    ['$LuaDanshou1'] = '此城危难，我必当竭尽全力！',
+    ['$LuaDanshou2'] = '大丈夫，屈伸有道，不在一时胜负',
+    ['~JieZhuran'] = '大耳贼就在眼前，快追……',
+    -- 界李儒
+    ['$LuaJuece1'] = '束手就擒吧！',
+    ['$LuaJuece2'] = '斩草除根，以绝后患！',
+    ['$LuaMieji1'] = '就是要让你无路可走！',
+    ['$LuaMieji2'] = '你也逃不了！',
+    ['$LuaFencheng1'] = '千里皇城，尽作焦土！',
+    ['$LuaFencheng2'] = '荣耀、权利、欲望，统统让这大火焚灭吧！',
+    ['~JieLiru'] = '吾等，皆死于妇人之手矣！',
+    -- 界满宠
+    ['$LuaJunxing1'] = '严法尚公，岂分贵贱而异施？',
+    ['$LuaJunxing2'] = '情理可容之事，法未必能容！',
+    ['$LuaYuce1'] = '骄之以利，示之以慑',
+    ['$LuaYuce2'] = '虽举得于外，则福生于内矣',
+    ['~JieManchong'] = '宠一生为公，无愧忠俭之节',
+}
