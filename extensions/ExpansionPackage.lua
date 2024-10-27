ecacbdc007ee90abab529d6b51d9f7f04d46ce33
Rinsan <cyjdtxz@gmail.com>
2024-10-27 16:05:27 +0800

    fix: 补充指示效果

--- extensions/ExpansionPackage.lua
+++ extensions/ExpansionPackage.lua
@@ -8165,6 +8165,7 @@ LuaYimou = sgs.CreateTriggerSkill {
                     end
                 else
                     -- 交给别的角色一张牌，然后摸一张牌
+                    room:doAnimate(rinsan.ANIMATE_INDICATE, baoxin:objectName(), victim:objectName())
                     room:askForYiji(victim, victim:handCards(), self:objectName(), false, false, false, 1)
                     victim:drawCards(1, self:objectName())
                 end
