2addbff56af1cca0736ad7d454448b86e58d41d7
Rinsan <cyjdtxz@gmail.com>
2025-02-08 01:38:09 +0800

    feat: 追加语音

---
+++ lang/zh_CN/Audio/GatheringHeroesFarAwayJianghuPackageLines.lua
@@ -0,0 +1,8 @@
+-- lines for Gathering Heroes Far Away Jianghu Package
+
+return {
+    -- 管宁
+    ['$LuaDunshi1'] = '失路青山隐，藏名白水游',
+    ['$LuaDunshi2'] = '隐居青松畔，遁走孤竹丘',
+    ['~ExTenYearGuanning'] = '高节始终，无憾矣',
+}
